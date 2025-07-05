58a92ce0a0fb53f96c70f578bb1a76d2062d4e3b
Donia Hussien <104635728+doniahusien@users.noreply.github.com>
2025-07-06 02:41:45 +0300

    Delete src directory

--- src/data/mainlinks.jsx
+++
@@ -1,8 +0,0 @@
-const mainLinks = [
-    { label: "Home", href: "/" },
-    { label: "About", href: "/about" },
-    { label: "Patient", href: "/patient" },
-    { label: "Market", href: "/market" },
-    { label: "Contact Us", href: "/contact" },
-]
-export default mainLinks;
